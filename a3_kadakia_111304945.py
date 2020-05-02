653c87c38662293de30a890a705946b30cb96744
Avik Kadakia <avik.kadakia@stonybrook.edu>
2020-05-01 20:38:19 -0400

    FInished part 1. Need to improve the statistics

--- a3_kadakia_111304945.py
+++ a3_kadakia_111304945.py
@@ -8,7 +8,6 @@ import torch
 import sys
 import pandas as pd
 from nltk.tokenize import word_tokenize
-import time
 import multiprocessing
 
 
@@ -47,42 +46,69 @@ def readCSV(fileName):
         reviews.append(word_tokenize(
             line[5].lower()) if type(line[5]) == str else "")
 
-        # print("\nItem id:", line[0], type(line[0]), "Tokenized", item_id_tokenized)
-        # print("Item rating:", line[1], type(line[1]), "Tokenized", rating_tokenized)
-        # print("Item review:", line[5], type(line[5]), "Tokenized", review_tokenized)
-
     return [item_ids, reviews, ratings]
 
 
 def trainWord2VecModel(reviews):
+    """
+    Trains a Word2Vec model from the given reviews.
+
+    Attributes
+    ----------
+    reviews : list
+        A list of all the reviews.
+
+    Returns
+    -------
+    w2v_model : Word2Vec model
+        A trained Word2Vec model.
+
+    Steps
+    -----
+    Step 1.3: Use GenSim word2vec to train a 128-dimensional word2vec model utilizing only the training data.
+    """
+
     cores = multiprocessing.cpu_count()
 
-    w2v_model = Word2Vec(min_count=3,
-                         window=3,
-                         size=128,
-                         alpha=0.03,
+    w2v_model = Word2Vec(sentences=reviews,
+                         workers=cores-1,
+                         window=10,
+                         negative=0,
+                         sample=1e-5,
+                         min_count=3,
                          seed=42,
                          iter=10,
-                         workers=cores-1)
+                         size=128)
 
-    t = time.time()
-
-    w2v_model.build_vocab(reviews, progress_per=1000)
-
-    print('Time to build vocab: {} mins'.format((time.time() - t) / 60))
-
-    t = time.time()
-
-    w2v_model.train(reviews, total_examples=w2v_model.corpus_count,
-                    epochs=30, report_delay=0)
-
-    print('Time to train the model: {} mins'.format((time.time() - t) / 60))
+    w2v_model.train(sentences=reviews, 
+                    total_examples=w2v_model.corpus_count,
+                    epochs=10)
 
     return w2v_model
 
 
 def getFeatures(w2v_model, reviews):
-    ridge_train_x = []
+    """
+    Returns the extracted features from the Word2Vec model.
+
+    Attributes
+    ----------
+    w2v_model : Word2Vec model
+        A trained Word2Vec model.
+    reviews : list
+        A list of all the reviews.
+
+    Returns
+    -------
+    train_x : Numpy Array
+        An array containing the averaged extracted features from the Word2Vec model.
+
+    Steps
+    -----
+    Step 1.4: Extract features: utilizing your word2vec model, get a representation for each word per review.
+    """
+
+    train_x = []
 
     for review in reviews:
         temp = [0] * 128
@@ -92,12 +118,35 @@ def getFeatures(w2v_model, reviews):
                 temp += w2v_model.wv[word]
 
         temp = np.asarray(temp)
-        ridge_train_x.append(temp/len(review))
+        train_x.append(temp/len(review))
 
-    return np.asarray(ridge_train_x)
+    return np.asarray(train_x)
 
 
 def buildRatingPredictor(train_x, test_x, train_y, test_y):
+    """
+    Tries different models and returns the one wit the best accuracy.
+
+    Attributes
+    ----------
+    train_x : Numpy array
+        The data for training the model.
+    test_x : Numpy array
+        The data for testing the model.
+    train_y : Numpy array
+        The data for training the model.
+    test_y : Numpy array
+        The data for training the model.
+
+    Returns
+    -------
+    bestModel : Ridge model
+        A trained Ridge model.
+
+    Steps
+    -----
+    Step 1.5: Build a rating predictor using L2 *linear* regression (can use the SKLearn Ridge class) with word2vec features.
+    """
 
     listC = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
     maxAccuracy = -1
@@ -111,7 +160,7 @@ def buildRatingPredictor(train_x, test_x, train_y, test_y):
         acc = MAE(test_y, pred_y_test)
 
         if acc >= maxAccuracy and acc < 1:
-            print("Current Best Model acc:", acc, "C:", C)
+            # print("Current Best Model acc:", acc, "C:", C)
             maxAccuracy = acc
             bestModel = model
 
@@ -124,11 +173,7 @@ def buildRatingPredictor(train_x, test_x, train_y, test_y):
 
 if __name__ == '__main__':
 
-    print("Welcome to the project")
-
-    now = time.time()
-
-    print("Stage 1.1: Read the reviews and ratings from the file.\n")
+    print("Stage 1:\n")
 
     if(len(sys.argv) < 3 or len(sys.argv) > 3):
         print("Please enter the right amount of arguments in the following manner:\npython3 a3_kadakia_111304945 '*_train.csv' '*_trial.csv'")
@@ -139,32 +184,44 @@ if __name__ == '__main__':
         training_file = sys.argv[1]
         trial_file = sys.argv[2]
 
+    # Stage 1.1: Read the reviews and ratings from the file
     training_data = readCSV(training_file)
-
     trial_data = readCSV(trial_file)
 
+    # Stage 1.3: Use GenSim word2vec to train a 128-dimensional word2vec model utilizing only the training data
     train_w2v_model = trainWord2VecModel(training_data[1])
     test_w2v_model = trainWord2VecModel(trial_data[1])
 
+    # Stage 1.4: Extract features
     train_x = getFeatures(train_w2v_model, training_data[1])
     test_x = getFeatures(test_w2v_model, trial_data[1])
-
     train_y = np.asarray(training_data[2], dtype=np.int)
     test_y = np.asarray(trial_data[2], dtype=np.int)
 
+    # Stage 1.5: Build a rating predictor
     rating_model = buildRatingPredictor(train_x, test_x, train_y, test_y)
-    pred_y_test = rating_model.predict(test_x)
-    pred_y_train = rating_model.predict(train_x)
+    y_pred = rating_model.predict(test_x)
+    
+    # Stage 1.6: Print both the mean absolute error and Pearson correlation between the predictions and the (test input) set
+    print("\nAccuracy for the Ridge model test:", MAE(test_y, y_pred))
 
-    print(len(train_w2v_model.wv.vocab))
+    # print()
+    print("\nAccuracy for the Pearson test:", ss.pearsonr(test_y, y_pred))
+    print()
+    print("\nStage 1 Checkpoint:\n")
+    print("MAE (test):", MAE(test_y, y_pred))
+    print("Pearson product-moment correlation coefficients (test):", np.corrcoef(test_y, y_pred))
 
-    print()
-    print("Accuracy for the Ridge model train:", MAE(train_y, pred_y_train))
-    print("Accuracy for the Ridge model test:", MAE(test_y, pred_y_test))
+    if "food" in trial_file:
+        testCases = [548, 4258, 4766, 5800]
 
-    print()
-    print("Accuracy for the Pearson train:",
-          ss.pearsonr(train_y, pred_y_train))
-    print("Accuracy for the Pearson test:", ss.pearsonr(test_y, pred_y_test))
-    print()
-    print("Total time:", time.time() - now)
+        for case in testCases:
+            if case in trial_data[0]:
+                pos = trial_data[0].index(case)
+                print()
+                print(case, "\tPredicted Value",
+                      y_pred[pos], "\tTrue Value:", trial_data[2][pos])
+            else:
+                print(case, "not in", trial_file)
+    
+    print("Stage 2:\n")
